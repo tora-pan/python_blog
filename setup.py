ca4ea7ab4f682c1daea4fbe7c78127a276c33310
tora_pan <travis.pandos@gmail.com>
2021-09-30 20:51:41 -0700

    add setup.py and models for db

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+requires = (
+    "flask",
+    "flask-sqlalchemy",
+    "flask_wtf",
+    "WTForms",
+    "email-validator"
+)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python_blog",
+    author = "Travis Pandos",
+    author_email = "travis.pandos@gmail.com",
+    license = "MIT",
+    packages=find_packages(),
+    install_requires=requires,
+)
